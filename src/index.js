0abb27582972acf879e104aa54b459ceef419f3b
IzaFreire <kariny.frei.2000@gmail.com>
2023-01-09 06:30:46 -0300

    Requisto 2 - tweets

--- src/index.js
+++ src/index.js
@@ -23,5 +23,26 @@ app.post("/sign-up", (req, res) => {
     res.status(201).send({ message: "OK" });
 });
 
+app.post("/tweets", (req, res) => {
+  const { user } = req.headers;
+  const { tweet } = req.body;
+
+  if (!user) {
+    res.status(400).send({ error: "Envie o username" });
+    return;
+  }
+
+  if (!tweet) {
+    res.status(400).send({ error: "Envie um tweet!" });
+    return;
+  }
+
+  tweets.push({ username: user, tweet: `${tweet} ${tweets.length + 1}` });
+
+  res.status(201).send({ message: "OK" });
+});
+
+
+
 
 app.listen(5000, () => console.log("Server running in port: 5000"));
